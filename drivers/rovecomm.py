190f31446b10d7ffd7263d0a4fd17c69c29d210f
Owen Chiaventone <omc8db@mst.edu>
2017-02-11 20:47:07 -0600

    Document python rovecomm, begin adding advanced features

--- drivers/rovecomm.py
+++ drivers/rovecomm.py
@@ -1,41 +1,95 @@
 import socket
 import struct
 import threading
+import logging
 
-PORT = 11000
+PORT          = 11000
+VERSION       = 1
+HEADER_FORMAT = ">BHBHH"
+
+ACK_FLAG      = 0x01
+INTERNAL_DATA_IDS = {
+    "Ping"       : 1,
+    "Ping Reply" : 2,
+    "Subscribe"  : 3,
+
+}
+
 
 class RoveComm(object):
-    header_format = ">BHBHH"
-    version = 1
+    """
+    RoveComm message sender and receiver
+
+    Example:
+
+        from rovecomm import RoveComm
+        import struct
+        import time
+        import random
+
+        def set_speed_handler(contents):
+            # Contents are typically C style structs
+            # ">HH" is a format code for two uint16_t
+            # See python's documentation for struct.unpack
+            # for more info about format codes
+            # And working with C style structs from python
+            speed_left, speed_right = struct.unpack(">HH", contents)
+            print "Speed set to %d, %d" % (speed_left, speed_right)
+
+        def add_waypoint_handler(contents):
+            latitude, longitude = struct.unpack(">dd")
+            print "Added waypoint (%f, %f) " % (latitude, longitude)
+
+        rovecomm_node = RoveComm()
+
+        # use RoveComm.callbacks to define what code should
+        # run when a message is received.
+        # Here we assign data id 138 to set_speed_handler
+        # and data id 267 to add_waypoint_handler
+
+        rovecomm_node.callbacks[138] = set_speed_handler
+        rovecomm_node.callbacks[267] = add_waypoint_handler
+
+        # Now you can do the rest of your program
+        while True:
+            my_telemetry = random.random()
+            rovecomm_node.send(data_id = 138,
+                               data = struct.pack(my_telemetry, 'f'),
+                               destination_ip = "192.168.1.20")
+            time.sleep(5)
+    """
 
     def __init__(self):
         self.callbacks = {}
 
         try:
-            self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            self.socket.bind(("127.0.0.1", RoveComm.port))
-        except:
-            print "Error: Could not claim port." \
-                  " You can only have one RoveComm listener at a time " \
-                  " Are you sure you didn't accidentally make a copy? "
-            raise Exception()
+            self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self._socket.bind(("127.0.0.1", PORT))
+        except socket.error:
+            raise Exception("Error: Could not claim port. "
+                            "Either another program or another copy or rovecomm"
+                            "is using port %d " % (PORT,))
 
-        #start a thread / async thingy
-        self.monitoring_thread = threading.Thread(target=self._listen_thread)
-        self.monitoring_thread.daemon = True
-        self.monitoring_thread.start()
+        # start a thread to get messages in the background
+        self._monitoring_thread = threading.Thread(target=self._listen_thread)
+        self._monitoring_thread.daemon = True
+        self._monitoring_thread.start()
 
-    def send(self, data, data_id, dest_ip, PORT, seq_num=0x0F49, flags=0x00):
-        """ Send a RoveComm formatted message
+    def send(self, data_id, data, destination_ip, seq_num=0x0F49, flags=0x00, port=PORT):
+        """
+        Send a RoveComm formatted message
 
         Parameters
         -----------
-            dataID: Short unsigned integer
-            data: bytearray or bytestring
-            dest_ip: IP address to send to. String formatted like "192.168.1.1"
-            port: Port number. 2 byte unsigned integer
-            seqNum: Short unsigned integer. Not used in current revision of RoveComm
-            flags: Unused in current version of RoveComm
+            data_id        : RoveComm Data ID. 16-bit integer.
+            data           : bytes to send
+            destination_ip : IP address to send to. String formatted like "192.168.1.1"
+
+            The following parameters are optional and not commonly used:
+
+            seq_num        : 16 bit sequence number. Not commonly used.
+            port           : Port number. 2 byte unsigned integer
+            flags          : Bit addressable field. OR all ack flags together
         """
 
         packet_size = len(data)
@@ -46,12 +100,12 @@ class RoveComm(object):
                              data_id,
                              packet_size)
         msgbuffer = header + data
-        # print "Message Buffer: ", binascii.hexlify(msgbuffer), "\n"
-        self.socket.sendto(msgbuffer, (dest_ip, port))
+        self._socket.sendto(msgbuffer, (destination_ip, port))
 
     def _listen_thread(self):
         while True:
-            packet, addr = self.socket.recvfrom(1024)
+            packet, addr = self._socket.recvfrom(1024)
+            # TODO: Upgrade to logging
             print "DEBUG: Packet received: ", packet
 
             data_id, content_bytes = self._parse_header(packet)
