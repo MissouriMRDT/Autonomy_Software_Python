563692bfe52f96ef2fc602e7ec10d8ceddc39a08
Owen Chiaventone <omc8db@mst.edu>
2017-02-18 20:51:41 +0000

    Ball tracker verified on hardware. underdamped response

--- follow_ball.py
+++ follow_ball.py
@@ -6,9 +6,10 @@ motors = drivers.motorsRoveComm.Motors()
 
 WIDTH           = 640.0  # pixels
 FIELD_OF_VIEW   = 40.0   # degrees
-TARGET_DISTANCE = 1.0  # meters
+TARGET_DISTANCE = 0.4  # meters
 RADIUS          = .063 # meters
 SCALING_FACTOR  = 10.0 # pixel-meters
+POWER           = 20
 
 while True:
     ball_in_frame, center, radius = tracker.track_ball()
@@ -18,9 +19,9 @@ while True:
         print "Distance: %f" % distance
         if distance > TARGET_DISTANCE:
             print "Moving forward: %f" % angle_to_ball
-            motors.move(10, angle_to_ball)
+            motors.move(POWER, angle_to_ball)
         if distance < TARGET_DISTANCE:
             print "Moving backward: %f" % angle_to_ball
-            motors.move(-10, angle_to_ball)
+            motors.move(-POWER, angle_to_ball)
     else:
         print "no ball detected"
