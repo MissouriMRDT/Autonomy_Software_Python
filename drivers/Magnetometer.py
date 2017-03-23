1284e4085c1ef8f70fd0b5aab7f86956979e2887
Owen Chiaventone <omc8db@mst.edu>
2017-03-22 21:28:29 -0500

    Bugfix: trig error in autocalibration

--- drivers/Magnetometer.py
+++ drivers/Magnetometer.py
@@ -3,6 +3,7 @@ import struct
 import time
 import json
 import math
+from numpy import interp
 
 MAG_IP_ADDRESS = '192.168.1.133'
 MAG_DATA_ID = 1316
@@ -19,6 +20,8 @@ class Compass:
         try:
             with open(calibration_file) as calfile:
                 self._calibration = json.load(calfile)
+                self.x_range = [self._calibration['min_x'],self._calibration['max_x']
+                self.y_range = [self._calibration['min_y'],self._calibration['max_y']
         except IOError:
             print "Error: No calibration data available"
         except ValueError:
@@ -34,9 +37,10 @@ class Compass:
     def heading(self):
         x, y, z = self._coordinates
         cal = self._calibration
-        x_adj = (x - cal['min_x']) / (cal['max_x'] - cal['min_x'])
-        y_adj = (y - cal['min_y']) / (cal['max_y'] - cal['min_y'])
-        heading = math.atan(x_adj / y_adj)
+        x_adj = interp(x, self.x_range, [-1,1])
+        y_adj = interp(y, self.y_range, [-1,1])
+        heading = math.degrees(math.atan2(y_adj, x_adj))
+        heading = heading + 360 if heading < 0 else heading
         return heading
 
     def raw_xyz(self):
@@ -49,6 +53,7 @@ if __name__ == '__main__':
     """
 
     import thread
+    import numpy
     rovecomm_node = RoveComm()
     def do_nothing(packet_contents):
         pass
@@ -69,26 +74,28 @@ if __name__ == '__main__':
             y_meas.append(reading[1])
             time.sleep(0.3)
 
+    choice = raw_input("Calibrate the magnetometer (y/n)?")
+    if(choice == 'y'):
+        print("Press enter to start calibration. Press enter again when facing due north after turning at least 360 degrees")
+        raw_input()
+        thread.start_new_thread(sample_magnetometer, (x_meas, y_meas))
+        raw_input()
+        done_measuring = True
 
-    print("Press enter to start calibration. Press enter again when facing due north after turning at least 360 degrees")
-    raw_input()
-    thread.start_new_thread(sample_magnetometer, (x_meas, y_meas))
-    raw_input()
-    done_measuring = True
+        cal = {"min_x": min(x_meas), "max_x": max(x_meas), "min_y": min(y_meas), "max_y": max(y_meas)}
+        (x, y, z) = mag.raw_xyz()
+        x_adj = numpy.interp(x, [cal["min_x"], cal["max_x"]], [-1, 1])
+        y_adj = numpy.interp(y, [cal["min_y"], cal["max_y"]], [-1, 1])
+        heading = math.degrees(math.atan2(y_adj, x_adj))
+        heading = heading + 360 if heading < 0 else heading
+        cal['due_north_offset'] = heading
+        print("Due north offset: %f" % heading)
+        with open('mag_calibration.json', 'w') as calfile:
+            json.dump(cal, calfile)
 
-    cal = {"min_x": min(x_meas), "max_x": max(x_meas), "min_y": min(y_meas), "max_y": max(y_meas)}
-    (x, y, z) = mag.raw_xyz()
-    x_adj = (x - cal['min_x']) / (cal['max_x'] - cal['min_x'])
-    y_adj = (y - cal['min_y']) / (cal['max_y'] - cal['min_y'])
-    heading = math.atan(x_adj / y_adj)
-    cal['due_north_offset'] = heading
-    print("Due north offset: %f" % heading)
-    with open('mag_calibration.json', 'w') as calfile:
-        json.dump(cal, calfile)
-
-    print("Calibration completed.")
-    print("Press enter to display heading, ctrl-c to exit\n")
-    raw_input()
+        print("Calibration completed.")
+        print("Press enter to display heading, ctrl-c to exit\n")
+        raw_input()
 
     mag = Compass(rovecomm_node)
 
