3772456fbba2ff00bbc8334b1f96500013e7bc25
Megan Cash <mecfkc@mst.edu>
2018-05-01 18:09:14 -0500

    Color values changed for accuracy

--- algorithms/objecttracking.py
+++ algorithms/objecttracking.py
@@ -3,8 +3,8 @@ import cv2
 import time
 import threading
 
-GREEN_LOWER = np.array(cv2.cv.Scalar(29, 86, 6))
-GREEN_UPPER = np.array(cv2.cv.Scalar(64, 255, 255))
+GREEN_LOWER = np.array(cv2.cv.Scalar(77, 92, 14))
+GREEN_UPPER = np.array(cv2.cv.Scalar(255, 254, 202))
 MIN_RADIUS  = 20
 FRAME_RATE  = 10
 
@@ -21,14 +21,14 @@ class ObjectTracker(object):
         grabbed, self.framebuffer = self.camera.read()
         if not grabbed:
             print "Frame capture failed"
-    
+
         fourcc = cv2.cv.CV_FOURCC(*'XVID')
         video_filename = 'logs/objtracker_%s.avi' % time.strftime("%Y%m%d-%H%M%S")
         print(video_filename)
         self.video_out = cv2.VideoWriter(video_filename, fourcc, FRAME_RATE, (640, 480))
         assert(self.video_out.isOpened())
         self.recording_thread = threading.Thread(target=self._record_thread)
-    
+
     def __del__(self):
         self.camera.release()
         self.video_out.release()
@@ -59,11 +59,11 @@ class ObjectTracker(object):
             if radius > MIN_RADIUS:
                 cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
                 cv2.circle(frame, center, 5, (0, 0, 255), -1)
-                
+
         self.video_out.write(frame)
 
         return ball_in_frame, center, radius
-    
+
     def _record_thread(self):
         pass
         #while(True):
