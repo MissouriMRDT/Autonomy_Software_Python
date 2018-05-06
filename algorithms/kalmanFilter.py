a3855ef6b52cd8ef2d1a9278e2e59b95f4738119
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-05-06 04:11:09 -0500

    Stripped extra comments, commented not useful math for the time being. Generates fairly accurate heading value

--- algorithms/kalmanFilter.py
+++ algorithms/kalmanFilter.py
@@ -19,12 +19,12 @@ R_angle = 0.005
 # Use calibrateBerryIMU.py to get calibration values
 # Calibrating the compass isnt mandatory, however a calibrated
 # compass will result in a more accurate heading values.
-magXmin = -0.49546000361442566
-magYmin = 0.12809999287128448
-magZmin = -0.1325799971818924
-magXmax = 0.20958000421524048
-magYmax = 0.8580600023269653
-magZmax = 0.5951399803161621
+magXmin = -0.42965999245643616
+magYmin = -0.2641800045967102
+magZmin = -0.5138000249862671
+magXmax = 0.5223399996757507
+magYmax = 0.6645799875259399
+magZmax = 0.4537400007247925
 
 
 class KalmanFilter:
@@ -128,6 +128,12 @@ class KalmanFilter:
 
         return self.KFangleX
 
+    def interp(self, x, inrange, outrange):
+        inspan = inrange[1] - inrange[0]
+        outspan = outrange[1] - outrange[0]
+        scaled = (x - inrange[0]) / inspan
+        return (scaled * outspan) + outrange[0]
+
     def calculateLoop(self):
     
         while True:
@@ -148,18 +154,18 @@ class KalmanFilter:
             LP = b.microseconds/(1000000*1.0)
 
             # Convert Gyro raw to degrees per second
-            rate_gyr_x = GYRx * G_GAIN
-            rate_gyr_y = GYRy * G_GAIN
-            rate_gyr_z = GYRz * G_GAIN
+            #rate_gyr_x = GYRx * G_GAIN
+            #rate_gyr_y = GYRy * G_GAIN
+            #rate_gyr_z = GYRz * G_GAIN
 
             # Calculate the angles from the gyro.
-            self.gyroXangle += rate_gyr_x*LP
-            self.gyroYangle += rate_gyr_y*LP
-            self.gyroZangle += rate_gyr_z*LP
+            #self.gyroXangle += rate_gyr_x*LP
+            #self.gyroYangle += rate_gyr_y*LP
+            #self.gyroZangle += rate_gyr_z*LP
 
             # Convert Accelerometer values to degrees
-            AccXangle = (math.atan2(ACCy, ACCz)+M_PI)*RAD_TO_DEG
-            AccYangle = (math.atan2(ACCz, ACCx)+M_PI)*RAD_TO_DEG
+            #AccXangle = (math.atan2(ACCy, ACCz)+M_PI)*RAD_TO_DEG
+            #AccYangle = (math.atan2(ACCz, ACCx)+M_PI)*RAD_TO_DEG
 
             ####################################################################
             ######################Correct rotation value########################
@@ -169,46 +175,25 @@ class KalmanFilter:
             #
             # Two different pieces of code are used depending on how your IMU is mounted.
             # If IMU is up the correct way, Skull logo is facing down, Use these lines
-            AccXangle -= 180.0
-            if AccYangle > 90:
-                AccYangle -= 270.0
-            else:
-                AccYangle += 90.0
-            #
-            #
-            #
-            #
-            # If IMU is upside down E.g Skull logo is facing up;
-            # if AccXangle >180:
-                #        AccXangle -= 360.0
-            # AccYangle-=90
-            # if (AccYangle >180):
-                #        AccYangle -= 360.0
-            ############################ END ##################################
+            #AccXangle -= 180.0
+            #if AccYangle > 90:
+            #    AccYangle -= 270.0
+            #else:
+            #    AccYangle += 90.0
+            
 
             # Complementary filter used to combine the accelerometer and gyro values.
-            self.CFangleX = AA*(self.CFangleX+rate_gyr_x*LP) + (1 - AA) * AccXangle
-            self.CFangleY = AA*(self.CFangleY+rate_gyr_y*LP) + (1 - AA) * AccYangle
+            #self.CFangleX = AA*(self.CFangleX+rate_gyr_x*LP) + (1 - AA) * AccXangle
+            #self.CFangleY = AA*(self.CFangleY+rate_gyr_y*LP) + (1 - AA) * AccYangle
 
             # Kalman filter used to combine the accelerometer and gyro values.
-            self.kalmanY = self.kalmanFilterY(AccYangle, rate_gyr_y, LP)
-            self.kalmanX = self.kalmanFilterX(AccXangle, rate_gyr_x, LP)
-
-            ####################################################################
-            ############################MAG direction ##########################
-            ####################################################################
-            # If IMU is upside down, then use this line.  It isnt needed if the
-            # IMU is the correct way up
-            #MAGy = -MAGy
-            #
-            ############################ END ##################################
+            #self.kalmanY = self.kalmanFilterY(AccYangle, rate_gyr_y, LP)
+            #self.kalmanX = self.kalmanFilterX(AccXangle, rate_gyr_x, LP)
+            
 
             # Calculate heading
-            head = 180 * math.atan2(MAGy, MAGx)/M_PI
 
-            # Only have our heading between 0 and 360
-            if head < 0:
-                head += 360
+            head = (180 * math.atan2(MAGz, MAGx)/M_PI) % 360
 
             self.heading = head
 
@@ -220,33 +205,17 @@ class KalmanFilter:
             self.accZnorm = ACCz/math.sqrt(ACCx * ACCx + ACCy * ACCy + ACCz * ACCz)
 
             # Calculate pitch and roll
-            # Use these two lines when the IMU is up the right way. Skull logo is facing down
             self.roll = math.asin(self.accXnorm)
             self.pitch = math.asin(self.accZnorm/math.cos(self.roll))
 
-            # Us these four lines when the IMU is upside down. Skull logo is facing up
-            # accXnorm = -accXnorm				#flip Xnorm as the IMU is upside down
-            # accYnorm = -accYnorm				#flip Ynorm as the IMU is upside down
-            #pitch = math.asin(accXnorm)
-            #roll = math.asin(accYnorm/math.cos(pitch))
-            #
             # Calculate the new tilt compensated values
-            magXcomp = MAGx*math.cos(self.pitch)+MAGz*math.sin(self.pitch)
+            magXcomp = MAGx*math.cos(self.pitch)+MAGy*math.sin(self.pitch)
 
-            # The compass and accelerometer are orientated differently on the LSM9DS0 and LSM9DS1 and the Z axis on the compass
-            # is also reversed. This needs to be taken into consideration when performing the calculations
-            if(False):
-                magYcomp = MAGx*math.sin(self.roll)*math.sin(self.pitch)+MAGy * \
-                    math.cos(self.roll)-MAGz*math.sin(self.roll)*math.cos(self.pitch)  # LSM9DS0
-            else:
-                magYcomp = MAGx*math.sin(self.roll)*math.sin(self.pitch)+MAGy * \
-                    math.cos(self.roll)+MAGz*math.sin(self.roll)*math.cos(self.pitch)  # LSM9DS1
+            magZcomp = MAGx*math.sin(self.roll)*math.sin(self.pitch)+MAGz * \
+                math.cos(self.roll)-MAGy*math.sin(self.roll)*math.cos(self.pitch)
 
             # Calculate tilt compensated heading
-            tiltCompensatedHeading = 180 * math.atan2(magYcomp, magXcomp)/M_PI
-
-            if tiltCompensatedHeading < 0:
-                tiltCompensatedHeading += 360
+            tiltCompensatedHeading = (180 * math.atan2(magZcomp, magXcomp)/M_PI) % 360
 
             self.headingCompensated = tiltCompensatedHeading
 
