fd3dbc6f36c2f552757af472f9ed1f4ec3ed5bd5
Michael Pieper <michaelepieper@gmail.com>
2018-09-29 17:46:35 -0500

    Switched some events to be handled by different states, new event for if the search pattern fails to find the marker

--- tests/state_test.py
+++ tests/state_test.py
@@ -3,9 +3,9 @@ import time
 
 
 def callback_func():
-    print "Running callback"
+    print("Running callback")
     time.sleep(TIME_INTERVAL)
-    print "Switched to state: " + str(stateSwitcher.state) + "\n"
+    print("Switched to state: " + str(stateSwitcher.state) + "\n")
 
 
 stateSwitcher = StateSwitcher()
@@ -13,13 +13,13 @@ TIME_INTERVAL = 5
 
 time.sleep(TIME_INTERVAL)
 
-print "Starting autonomy..."
+print("Starting autonomy...")
 stateSwitcher.handle_event(AutonomyEvents.START)
-print "Switched to state: " + str(stateSwitcher.state) + "\n"
+print("Switched to state: " + str(stateSwitcher.state) + "\n")
 
 time.sleep(TIME_INTERVAL)
 
-print "Rover reached a marker"
+print("Rover reached a marker")
 stateSwitcher.handle_event(AutonomyEvents.REACHED_MARKER, callback=callback_func)
 
 time.sleep(TIME_INTERVAL)
