9616159e39d15fffcaee460240ce3bb2a453a278
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-11-09 23:41:05 -0600

    Updated navboard driver for pitch, roll, heading output

--- drivers/nav_board.py
+++ drivers/nav_board.py
@@ -3,39 +3,36 @@ import struct
 import time
 
 NAV_IP_ADDRESS = "192.168.1.133"
-ACC_DATA_ID = 1314
-GYR_DATA_ID = 1315
-MAG_DATA_ID = 1316
+PITCH_DATA_ID = 1314
+ROLL_DATA_ID = 1315
+HEADING_DATA_ID = 1316
 GPS_DATA_ID = 1297
 
 
 class NavBoard:
     def __init__(self, rove_comm):
-        self._mag = (0, 0, 0)
-        self._acc = (0, 0, 0)
-        self._gyr = (0, 0, 0)
+        self._pitch = 0
+        self._roll = 0
+        self._heading = 0
         self._location = (0, 0)
 
         self.rove_comm_node = rove_comm
 
         self.rove_comm_node.subscribe(NAV_IP_ADDRESS)
 
-        self.rove_comm_node.callbacks[MAG_DATA_ID] = self.process_mag_data
-        self.rove_comm_node.callbacks[ACC_DATA_ID] = self.process_acc_data
-        self.rove_comm_node.callbacks[GYR_DATA_ID] = self.process_gyr_data
+        self.rove_comm_node.callbacks[PITCH_DATA_ID] = self.process_pitch_data
+        self.rove_comm_node.callbacks[ROLL_DATA_ID] = self.process_roll_data
+        self.rove_comm_node.callbacks[HEADING_DATA_ID] = self.process_heading_data
         self.rove_comm_node.callbacks[GPS_DATA_ID] = self.process_gps_data
 
-    def process_mag_data(self, raw_data):
-        x, y, z = struct.unpack("fff", raw_data)
-        self._mag = (x, y, z)
+    def process_pitch_data(self, raw_data):
+        self._pitch = struct.unpack("f", raw_data)
 
-    def process_acc_data(self, raw_data):
-        x, y, z = struct.unpack("fff", raw_data)
-        self._acc = (x, y, z)
+    def process_roll_data(self, raw_data):
+        self._roll = struct.unpack("f", raw_data)
 
-    def process_gyr_data(self, raw_data):
-        x, y, z = struct.unpack("fff", raw_data)
-        self._gyr = (x, y, z)
+    def process_heading_data(self, raw_data):
+        self._heading = struct.unpack("f", raw_data)
 
     def process_gps_data(self, raw_data):
         # The GPS sends data as two doubles
@@ -44,14 +41,14 @@ class NavBoard:
         lon = -lon * 1e-7
         self._location = (lat, lon)
 
-    def magnetometer_xyz(self):
-        return self._mag
+    def pitch(self):
+        return self._pitch
 
-    def accelerometer_xyz(self):
-        return self._acc
+    def roll(self):
+        return self._roll
 
-    def gyroscope_xyz(self):
-        return self._gyr
+    def heading(self):
+        return self._heading
 
     def location(self):
         return self._location
@@ -62,8 +59,8 @@ if __name__ == '__main__':
     nav = NavBoard(rove_comm_node)
     while True:
         print(nav.location())
-        print(nav.magnetometer_xyz())
-        print(nav.accelerometer_xyz())
-        print(nav.gyroscope_xyz())
+        print(nav.heading())
+        print(nav.pitch())
+        print(nav.roll())
         print("...")
         time.sleep(1)
