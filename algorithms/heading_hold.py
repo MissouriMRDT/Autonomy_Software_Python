a910e13679f2d3b8d86bb0e2bbd7fa75aad5715f
Nathan Skelton <nathan.hunter.skelton@gmail.com>
2018-12-01 16:39:51 -0600

    Resolved logic issue & extremely low speed

--- algorithms/heading_hold.py
+++ algorithms/heading_hold.py
@@ -1,5 +1,5 @@
 
 def get_motor_power_from_heading(goal_heading, drive_board, nav_board):
-    if abs(goal_heading - nav_board.heading()) < 5 or abs(nav_board.roll()) > 15:
-        return drive_board.calculate_move(10, goal_heading - nav_board.heading())
-    return 10, 10
+    if abs(goal_heading - nav_board.heading()) > 5:
+        return drive_board.calculate_move(50, goal_heading - nav_board.heading())
+    return 50, 50
