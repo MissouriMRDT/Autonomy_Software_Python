2a4c14a871a0c96e960dfa4d20d7ac4f122daecb
Michael Pieper <michaelepieper@gmail.com>
2018-12-10 18:57:48 -0600

    Condense search for marker

--- algorithms/marker_search.py
+++ algorithms/marker_search.py
@@ -1,26 +1,6 @@
-from enum import Enum
+from drivers.drive_board import DriveBoard
 
 
 # Performs a search pattern based on the provided argument
-def search_for_marker(search_pattern):
-    if search_pattern == SearchPattern.ARCHIMEDES:
-        archimedes_spiral_search()
-    elif search_pattern == SearchPattern.SQUARE:
-        square_spiral_search()
-    else:
-        print("Illegal argument in search_for_marker: Must be enum of SearchPattern")
-
-
-# Drives in an archimedes spiral
-def archimedes_spiral_search():
-    pass
-
-
-# Drives in a square spiral
-def square_spiral_search():
-    pass
-
-
-class SearchPattern(Enum):
-    ARCHIMEDES = 1
-    SQUARE = 2
+def search_for_marker(drive):
+    drive.calculate_move(1, 1)
