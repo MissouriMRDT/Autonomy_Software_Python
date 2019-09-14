c52c559ba67a7914e0cbb569e9f25c29d8ff362e
krikwenssej <19jnewkirk@gmail.com>
2019-09-14 13:16:03 -0500

    Revert "Delete state.py"
    This reverts commit 1cfad526878b49fdd41e2393e1ed133957467b20.

---
+++ state.py
@@ -0,0 +1,20 @@
+class RoverState(object):
+
+    def __init__(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def __str__(self):
+        return self.__class__.__name__
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def __eq__(self, other):
+        return str(self) == str(other)
+
+    def __ne__(self, other):
+        return str(self) != str(other)
+
