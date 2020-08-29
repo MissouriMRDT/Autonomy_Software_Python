7791f495a00121da74e098da5a0d0e5e948065a5
Eli Verbrugge <eli.verbrugge@gmail.com>
2020-08-29 15:56:48 -0500

    Switched search pattern unit test to use matplotlib

--- tests/unit/marker_search_test.py
+++ tests/unit/marker_search_test.py
@@ -1,4 +1,7 @@
-import turtle
+
+import numpy as np 
+import matplotlib.testing.compare as plt_tst
+import matplotlib.pyplot as plt 
 import algorithms.marker_search as marker_search
 import core.constants as constants
 
@@ -6,42 +9,16 @@ import core.constants as constants
 UNIT TEST
 FILE: marker_search.py
 
-DESCRIPTION: This file provides a unit test for the search pattern generation. It compares the output of a turtle (python) drawing
+DESCRIPTION: This file provides a unit test for the search pattern generation. It compares the output of a matplotlib (python) drawing
 to that of a previously correctly generated one. 
 
 PROCEDURES: If any changes are made, place this file in the root directory and run it directly to confirm and update the expected output.
 '''
 
-
-def compareOutputs(output, expected):
+def drawLogarithmicSpiral(r=50, fileName='resources/tests/output_search_pattern.png'):
     '''
-    Compare the two outputs to see if the marker_search
-    code still outputs the same pattern, we compare line by
-    line as these are PS files
+    Draws and outputs the pattern provided by marker_search using the matplotlib module
     '''
-
-    with open(output, 'r') as file1:
-        with open(expected, 'r') as file2:
-            diff = set(file1).difference(file2)
-
-    # the only difference between the files should be the creation date
-    if len(diff) == 1 and 'CreationDate' in next(iter(diff)):
-        for line in diff:
-            print(line)
-        return True
-
-    else:
-        for line in diff:
-            print(line)
-    return False
-
-
-def drawLogarithmicSpiral(r=50, fileName="resources/tests/output_search_pattern.eps"):
-    '''
-    Draws and outputs the pattern provided by marker_search using the turtle module
-    '''
-    t = turtle.Turtle()
-    t.up()
 
     x = 0
     y = 0
@@ -50,16 +27,13 @@ def drawLogarithmicSpiral(r=50, fileName="resources/tests/output_search_pattern.
     start_coord = constants.Coordinate(37.951424, -91.768959)
     end_coord = constants.Coordinate(37.951424, -91.768959)
 
-    t.setpos(x, y)
-    t.down()
-    t.hideturtle()
-    t.speed(0)
+    li = []
 
     for i in range(r):
         # generate the next point in spiral
         new_coord = marker_search.calculate_next_coordinate(start_coord, end_coord)
 
-        # calculate the deltas in position, we can't represent the precision of GPS coords in turtle
+        # calculate the deltas in position, we can't represent the precision of GPS coords in matplotlib
         dx = new_coord.lon - end_coord.lon
         dy = new_coord.lat - end_coord.lat
 
@@ -67,23 +41,23 @@ def drawLogarithmicSpiral(r=50, fileName="resources/tests/output_search_pattern.
         x += dx * 500000
         y += dy * 500000
 
-        t.setpos(x, y)
         end_coord = constants.Coordinate(new_coord.lat, new_coord.lon)
+        li.append((x, y))
 
-    # now export the turtle drawing to the resources folder
-    ts = t.getscreen()
-    ts.getcanvas().postscript(file=fileName)
+    plt.plot(*zip(*li))
+    plt.axis('off')
+    plt.savefig(fileName)
 
 
 def test_calculate_next_coordinate():
     '''
-    Compare the generated marker search with the expected, assert that they are the same
+    Draw pattern and make sure it is the same as expected
     '''
 
     drawLogarithmicSpiral()
-    assert compareOutputs('resources/tests/output_search_pattern.eps', 'resources/tests/expected_search_pattern.eps') == True
+    assert plt_tst.compare_images('resources/tests/expected_search_pattern.png', 'resources/tests/output_search_pattern.png', 0.001) == None
 
 
 if __name__ == "__main__":
-    drawLogarithmicSpiral(fileName="resources/tests/expected_search_pattern.eps")
-    turtle.mainloop()
+    drawLogarithmicSpiral(fileName='resources/tests/expected_search_pattern.png')
+    plt.show()
