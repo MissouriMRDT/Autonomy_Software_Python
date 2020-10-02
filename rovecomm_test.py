fe8e6e05cde75cfd967eec52a9f3bfe18ed258bb
Josh Lawson <jlcmk@umsystem.edu>
2020-10-01 23:39:14 -0500

    Changed the collection to initialize its children

--- rovecomm_test.py
+++ rovecomm_test.py
@@ -11,34 +11,30 @@ rovecomm_test.variable = 0
 
 
 def main() -> None:
-    core.rovecomm_node.callbacks[4242] = addCounter
+    core.rovecomm.callbacks[4242] = handlePacket
     packet = RoveCommPacket(4242, 'b', (1, 3), "", 11000)
     packet.SetIp('127.0.0.1')
-    core.rovecomm_node.write(packet)
+    core.rovecomm.write(packet, False)
 
-    RoveCommTCP = RoveCommEthernetTcp(HOST='127.0.0.1', PORT=11111)
-    RoveCommTCP.callbacks[4242] = addCounter
-    packet2 = RoveCommPacket(4242, 'b', (1, 3), "", 11111)
+    packet2 = RoveCommPacket(4242, 'b', (1, 4), "", 11111)
     packet2.SetIp('127.0.0.1')
-    RoveCommTCP.write(packet2)
+    core.rovecomm.write(packet2, True)
 
     # Test socket to try to send to RoveComm
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("127.0.0.1", 11111))
     rovecomm_packet = struct.pack(">BHBB", 2, 4242, 2, 0)
-    for i in (1, 3):
+    for i in (1, 5):
         rovecomm_packet = rovecomm_packet + struct.pack('>b', i)
 
-    print(threading.enumerate())
+    # print(threading.enumerate())
     for i in range(5):
         s.send(rovecomm_packet)
         time.sleep(.5)
         print(rovecomm_test.variable)
-    print("Closing Thread")
-    print(threading.enumerate())
-    core.rovecomm.collection.close_thread()
-    print(threading.enumerate())
+    core.rovecomm.close_thread()
 
 
-def addCounter(packet):
+def handlePacket(packet):
+    packet.print()
     rovecomm_test.variable += 1
