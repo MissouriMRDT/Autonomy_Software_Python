27dd65433e52b20af70a04f0c527fa2b2b5bff65
Josh Lawson <jlcmk@umsystem.edu>
2020-10-17 15:09:18 -0500

    Closes rovecomm thread at end of program

--- run.py
+++ run.py
@@ -53,12 +53,15 @@ def main() -> None:
         # Couldn't find module because file doesn't exist or tried to import
         # from package
         logger.error(f"Failed to import module '{args.file}'")
+        core.rovecomm.close_thread()
         exit(1)
     except NameError:
         # Successful import but module does not define main
         logger.error(f"{args.file}: Undefined reference to main")
+        core.rovecomm.close_thread()
         exit(1)
     else:
+        core.rovecomm.close_thread()
         exit(0)
 
 
