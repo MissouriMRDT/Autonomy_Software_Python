924188690be87402566ab4ddf14821801f806e3e
Josh Lawson <jlcmk@umsystem.edu>
2020-10-23 05:17:21 -0500

    Allowed csv handler to work with a single output file

--- core/logging_handlers.py
+++ core/logging_handlers.py
@@ -2,20 +2,28 @@ from core.rovecomm import RoveCommPacket
 import core
 import logging
 from datetime import datetime
+from os import path
 
 
 class CsvHandler(logging.handlers.WatchedFileHandler):
 
-    def __init__(self, filename, format_string, encoding=None, delay=False):
+    def __init__(self, filename, format_string, encoding=None, delay=False, new_file=False):
         """
         Initializes the handler
         """
-        timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
-        f = open(f'{filename[:-4]}-{timestamp}{filename[-4:]}', 'w')
-        f.write(format_string + '\n')
-        f.close()
 
-        logging.handlers.WatchedFileHandler.__init__(self, f'{filename[:-4]}-{timestamp}{filename[-4:]}', 'a', encoding, delay)
+        if (new_file):
+            timestamp = datetime.now().strftime("%Y%m%d-%H%M%S")
+            f = open(f'{filename[:-4]}-{timestamp}{filename[-4:]}', 'w')
+            f.write(format_string + '\n')
+            f.close()
+            logging.handlers.WatchedFileHandler.__init__(self, f'{filename[:-4]}-{timestamp}{filename[-4:]}', 'a', encoding, delay)
+        else:
+            if (not path.exists(filename)):
+                f = open(filename, 'w')
+                f.write(format_string + '\n')
+                f.close()
+            logging.handlers.WatchedFileHandler.__init__(self, filename, 'a', encoding, delay)
 
 
 class RoveCommHandlerUDP(logging.Handler):
