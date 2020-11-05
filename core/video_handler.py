44e3eacecc64f8868e2f57aa99e6e4819ea2101c
Eli Verbrugge <eli.verbrugge@gmail.com>
2020-11-05 17:42:07 -0600

    Added some more threading code, uncovering issues with threading and scheduling frames

--- core/video_handler.py
+++ core/video_handler.py
@@ -1,39 +1,103 @@
 import pyfakewebcam
 import cv2
 import time
+import pyzed.sl as sl
+import threading
+import time
+
+class Feed:
+    def __init__(self, mat, view, streamer=None, video_writer=None):
+        self.streamer = streamer
+        self.video_writer = video_writer
+        self.mat = mat
+        self.view = view
+    
 
 
 class VideoHandler:
     def __init__(self, resolution_x=640, resolution_y=480, frame_rate=30):
-        self.cameras = {}
-        self.video_writers = {}
-        self.fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        self.feeds = {}
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
         self.resolution_x = resolution_x
         self.resolution_y = resolution_y
         self.frame_rate = frame_rate
+        self.stopped = False
+
+        # Create a ZED camera object
+        self.zed = sl.Camera()
+
+        # Set configuration parameters
+        input_type = sl.InputType()
+    
+        init = sl.InitParameters(input_t=input_type)
+        init.camera_resolution = sl.RESOLUTION.HD720
+        init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
+        init.coordinate_units = sl.UNIT.MILLIMETER
+        init.camera_fps = 30
+
+
+        # Open the camera
+        err = self.zed.open(init)
+        if err != sl.ERROR_CODE.SUCCESS :
+            print(repr(err))
+            self.zed.close()
+            exit(1)
+
+        # Set runtime parameters after opening the camera
+        self.runtime = sl.RuntimeParameters()
+        self.runtime.sensing_mode = sl.SENSING_MODE.STANDARD
+
+        # Prepare new image size to retrieve half-resolution images
+        self.image_size = self.zed.get_camera_information().camera_resolution
+        self.image_size.height = self.image_size.height / 2
+        self.image_size.width = self.image_size.width /2 
+        self.update_thread = threading.Thread(target=self.update)
 
     def __del__(self):
         # Call close so we can close all the video writers
         self.close()
 
-    def add_video(self, camera_num, camera_id, save_video=True, stream_video=True):
-        if stream_video:
-            self.cameras[camera_id] = pyfakewebcam.FakeWebcam(f'/dev/video{camera_num}', self.resolution_x, self.resolution_y)  # append v4l output to list of cameras
+    def add_video(self, zed_view, zed_mat, camera_num, feed_id, save_video=True, stream_video=True):
+        # Create a Feed
+        feed = Feed(sl.Mat(self.image_size.width, self.image_size.height, zed_mat), zed_view)
 
+        if stream_video:
+            feed.streamer = pyfakewebcam.FakeWebcam(f'/dev/video{camera_num}', self.resolution_x, self.resolution_y)  # append v4l output to list of cameras
         if save_video:
-            video_filename = f'stream_{camera_id}_' + time.strftime("%Y%m%d-%H%M%S")  # save videos to unique files
-            self.video_writers[camera_id] = cv2.VideoWriter(video_filename + "_left.avi", self.fourcc, self.frame_rate, (self.resolution_x, self.resolution_y))  # append video writer to list of video writers
+            video_filename = f'stream_{feed_id}_' + time.strftime("%Y%m%d-%H%M%S")  # save videos to unique files
+            feed.video_writer = cv2.VideoWriter(video_filename + "_left.avi", self.fourcc, self.frame_rate, (self.resolution_x,self.resolution_y))  # append video writer to list of video writers
+        
+        # Add feed to our dictionary
+        self.feeds[feed_id] = feed
+    
+    def start(self):
+        self.update_thread.start()
 
     def close(self):
-        for key in self.video_writers:
-            self.video_writers[key].release()
+        self.stopped = True
+        for __, feed in self.feeds.items():
+            feed.video_writer.release()
+    
+    def update(self):
+        # Go through the existing feeds and read in new images and handle their 
+        err = self.zed.grab(self.runtime)
+        if err == sl.ERROR_CODE.SUCCESS :
+            for feed_id in self.feeds:
+                self.zed.retrieve_image(self.feeds[feed_id].mat, self.feeds[feed_id].view, sl.MEM.CPU, self.image_size)
+                self.handle_frames(self.feeds[feed_id])
 
-    def handle_frame(self, frame_id, frame):
+    def grab_frame(self, feed_id):
+        # Return the data for the latest frame in the specified feed
+        return self.feeds[feed_id].mat.get_data()
+
+    def handle_frames(self, feed):
         # Stream and save frames
-        if frame_id in self.cameras:
-            img = cv2.resize(frame, (640, 480))
-            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
-            self.cameras[frame_id].schedule_frame(img)
+        img = feed.mat.get_data()
+        img = cv2.resize(img, (640, 480))
+        if feed.streamer is not None:
+            image_resized = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+            feed.streamer.schedule_frame(image_resized)
 
-        if frame_id in self.video_writers:
-            self.video_writers.write(frame)
+        if feed.video_writer is not None:
+            image_resized = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
+            feed.video_writer.write(image_resized)
--- webcam_test.py
+++ webcam_test.py
@@ -1,77 +1,28 @@
-import time
-import pyfakewebcam
-import numpy as np
-import pyzed.sl as sl
-import os
-import cv2
 import core
+import logging
+import pyzed.sl as sl
+import time
 
-
-def setup_zed():
-    # Create a ZED camera object
-    zed = sl.Camera()
-
-    # Set configuration parameters
-    input_type = sl.InputType()
-   
-    init = sl.InitParameters(input_t=input_type)
-    init.camera_resolution = sl.RESOLUTION.HD720
-    init.depth_mode = sl.DEPTH_MODE.PERFORMANCE
-    init.coordinate_units = sl.UNIT.MILLIMETER
-    init.camera_fps = 30
-
-
-    # Open the camera
-    err = zed.open(init)
-    if err != sl.ERROR_CODE.SUCCESS :
-        print(repr(err))
-        zed.close()
-        exit(1)
-    return zed
-
-
-def main() :
-    zed = setup_zed()
+def main() -> None:
+    '''
+    Main function for example script, tests geomath code
+    '''
+    logger = logging.getLogger(__name__)
+    logger.info("Executing function: main()")
 
     # Add video streams to handler
-    core.video_handler.add_video(2, "regular")
-    core.video_handler.add_video(3, "depth")
+    core.video_handler = core.VideoHandler()
 
-    # Set runtime parameters after opening the camera
-    runtime = sl.RuntimeParameters()
-    runtime.sensing_mode = sl.SENSING_MODE.STANDARD
+    core.video_handler.add_video(sl.VIEW.DEPTH, sl.MAT_TYPE.U8_C4, 2, "regular")
+    core.video_handler.add_video(sl.VIEW.LEFT, sl.MAT_TYPE.U8_C4, 3, "depth")
+    #core.video_handler.start()
 
-    # Prepare new image size to retrieve half-resolution images
-    image_size = zed.get_camera_information().camera_resolution
-    image_size.height = image_size.height / 2
-    image_size.width = image_size.width /2 
- 
-
-    # Declare your sl.Mat matrices
-    image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
-    depth_image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
-
-    key = ' '
-    while key != 113 :
-        err = zed.grab(runtime)
-        if err == sl.ERROR_CODE.SUCCESS :
-            # Retrieve the left image, depth image in the half-resolution
-            zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, image_size)
-            zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
-
-            # To recover data from sl.Mat to use it with opencv, use the get_data() method
-            # It returns a numpy array that can be used as a matrix with opencv
-            image_ocv = image_zed.get_data()        
-            core.video_handler.handle_frame("regular", image_ocv)
-
-            image_depth = depth_image_zed.get_data()
-            core.video_handler.handle_frame("depth", image_depth)
-            
-    cv2.destroyAllWindows()
-    zed.close()
-
-    print("\nFINISH")
+    try:
+        while True:
+            core.video_handler.update()
+    except KeyboardInterrupt:
+        core.video_handler.close()
 
 if __name__ == "__main__":
-    # Call main()
+    # Run main()
     main()
