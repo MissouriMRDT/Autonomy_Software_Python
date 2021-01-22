cf29204c2ae76e2b4c55c5ccc834b7aba7745df3
Eli Verbrugge <eli.verbrugge@gmail.com>
2021-01-22 03:17:24 -0600

    Very WIP floor plane detector

--- core/vision/zed_handler.py
+++ core/vision/zed_handler.py
@@ -23,9 +23,7 @@ class ZedHandler:
         self.init.coordinate_units = sl.UNIT.METER
         self.init.camera_fps = 30
         self.init.depth_minimum_distance = 1
-        self.init.coordinate_system = (
-            sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP
-        )  # OpenGL coordinate system
+        self.init.coordinate_system = sl.COORDINATE_SYSTEM.RIGHT_HANDED_Y_UP  # OpenGL coordinate system
 
         # Open the camera
         err = self.zed.open(self.init)
@@ -62,12 +60,8 @@ class ZedHandler:
         self.depth_size.height = self.depth_size.height / 2
 
         # Declare your sl.Mat matrices
-        image_zed = sl.Mat(
-            self.image_size.width, self.image_size.height, sl.MAT_TYPE.U8_C4
-        )
-        depth_image_zed = sl.Mat(
-            self.depth_size.width, self.depth_size.height, sl.MAT_TYPE.U8_C4
-        )
+        image_zed = sl.Mat(self.image_size.width, self.image_size.height, sl.MAT_TYPE.U8_C4)
+        depth_image_zed = sl.Mat(self.depth_size.width, self.depth_size.height, sl.MAT_TYPE.U8_C4)
         self.zed.enable_positional_tracking()
 
         # Set runtime parameters after opening the camera
@@ -80,13 +74,9 @@ class ZedHandler:
             err = self.zed.grab(runtime)
             if err == sl.ERROR_CODE.SUCCESS:
                 # Grab images, and grab the data as opencv/numpy matrix
-                self.zed.retrieve_image(
-                    image_zed, sl.VIEW.LEFT, sl.MEM.CPU, self.image_size
-                )
+                self.zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, self.image_size)
                 self.reg_img = image_zed.get_data()
-                self.zed.retrieve_image(
-                    depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, self.depth_size
-                )
+                self.zed.retrieve_image(depth_image_zed, sl.VIEW.DEPTH, sl.MEM.CPU, self.depth_size)
                 self.depth_img = depth_image_zed.get_data()
 
                 # Now let the feed_handler stream/save the frames
@@ -107,9 +97,7 @@ class ZedHandler:
 
     def grab_depth_data(self):
         self.depth_map = sl.Mat()
-        self.zed.retrieve_measure(
-            self.depth_map, sl.MEASURE.DEPTH, sl.MEM.CPU, self.depth_size
-        )  # Retrieve depth
+        self.zed.retrieve_measure(self.depth_map, sl.MEASURE.DEPTH, sl.MEM.CPU, self.depth_size)  # Retrieve depth
         return self.depth_map
 
     def start(self):
@@ -142,9 +130,7 @@ class ZedHandler:
         Returns 3D point cloud data captured with ZED
         """
         point_cloud = sl.Mat()
-        self.zed.retrieve_measure(
-            point_cloud, sl.MEASURE.XYZ, sl.MEM.CPU, self.depth_size
-        )
+        self.zed.retrieve_measure(point_cloud, sl.MEASURE.XYZ, sl.MEM.CPU, self.depth_size)
         return point_cloud
 
     def get_floor(self):
@@ -158,3 +144,7 @@ class ZedHandler:
         info = self.zed.get_camera_information().calibration_parameters
         print(info)
         return info
+
+    def get_pose(self, pose):
+        tracking_state = self.zed.get_position(pose)
+        return
--- example/webcam_runner.py
+++ example/webcam_runner.py
@@ -21,7 +21,7 @@ def main() -> None:
         reg_img = core.vision.camera_handler.grab_regular()
         plane = core.vision.camera_handler.get_floor()
         # contours, hierarchy = cv2.findContours(reg_img, 2, cv2.CHAIN_APPROX_NONE)
-        print(plane.get_bounds())
+        # print(plane.get_bounds())
 
         cam_params = core.vision.camera_handler.get_info()
         K = np.array(
@@ -33,13 +33,32 @@ def main() -> None:
         )
 
         # Attempting to get camera matrix
-        pose = plane.get_pose()
+        pose = sl.Pose()
+        core.vision.camera_handler.get_pose(pose)
         R = pose.get_rotation_matrix().r.T
         t = pose.get_translation().get()
         world2cam = np.hstack((R, np.dot(-R, t).reshape(3, -1)))
+        print(world2cam)
+        print(K)
         P = np.dot(K, world2cam)  # camera matrix
 
-        print(plane.vertices)
+        points = []
+        bounds = plane.get_bounds()
+
+        print(bounds)
+        for i in bounds:
+            x = abs((i[0] * P[0][0]) / i[2] + P[0][2])
+            y = abs((i[1] * P[1][1]) / i[2] + P[1][2])
+            points.append([x, y])
+            # print(i)
+
+        print(points)
+        points = np.array(points).astype(np.int32)
+        print(points)
+        points = points.reshape((-1, 1, 2))
+
+        cv2.polylines(reg_img, [points], True, (0, 255, 255))
+
         # if mesh.get_boundaries != []:
         #    cv2.drawContours(reg_img, mesh.get_boundaries(), -1, (0, 255, 0), 3)
         # Display the camera frames we just grabbed (should show us if potential issues occur)
