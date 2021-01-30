3f1e63a1912b9e2acf4f72877689d1bc660e3880
EliVerbrugge <eli.verbrugge@gmail.com>
2021-01-30 13:54:37 -0600

    Adding rgb packet support

--- interfaces/multimedia_board.py
+++ interfaces/multimedia_board.py
@@ -1,5 +1,4 @@
 import core
-import time
 import logging
 
 
@@ -10,12 +9,16 @@ class MultiMedia:
     """
 
     def __init__(self):
-        # Set up RoveComm and Logger
+        # Set up logger
         self.logger = logging.getLogger(__name__)
 
     def send_lighting_state(self, state):
         """
         Writes a operation state to the lighting board to display on the LED matrix
+
+        Parameters:
+            state (enum) :  The operation state of the rover, defined in
+            core/constants.py
         """
         # Write a lighting state packet (TCP)
         core.rovecomm_node.write(
@@ -23,6 +26,25 @@ class MultiMedia:
                 core.manifest["Multimedia"]["Commands"]["StateDisplay"]["dataId"],
                 "B",
                 (state,),
+                core.manifest["Multimedia"]["Ip"],
+                core.UDP_OUTGOING_PORT,
+            ),
+            True,
+        )
+
+    def send_rgb(self, rgb):
+        """
+        Writes a rgb value to the LED matrix
+
+        Parameters:
+            rgb (tuple): the r, g, b values to be displayed on the LED matrix
+        """
+        # Write a lighting rgb packet (TCP)
+        core.rovecomm_node.write(
+            core.RoveCommPacket(
+                core.manifest["Multimedia"]["Commands"]["LEDRGB"]["dataId"],
+                "B",
+                rgb,
                 core.manifest["Multimedia"]["Ip"],
                 core.UDP_OUTGOING_PORT,
             ),
